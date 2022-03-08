798c8dce090c7c2bbc4fd3270f7abcd91bb0d4cc
Samuel Raducan <samuel.thembp@gmail.com>
2022-03-08 09:16:04 +0000

    Add sinatra reloader gem Co-authored-by: James Mooney <jam9304@users.noreply.github.com>

--- app.rb
+++ app.rb
@@ -1,5 +1,14 @@
 require 'sinatra'
+require 'sinatra/reloader' if development?
 
 get '/' do
   "Hello, world."
+end
+
+get '/secret' do
+  "Secret message"
+end
+
+get '/about' do
+  "About page!"
 end
